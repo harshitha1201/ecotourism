884edb73a2d7b868e7f31cf5d0341dbc86f34dc9
harshitha1201 <harshithadiddige24@gmail.com>
2025-04-04 12:12:39 +0530

    documentation written to get ?get_koala_data

--- R/get_koala_data.R
+++ R/get_koala_data.R
@@ -1,3 +1,13 @@
-fetch_koala_data <- function() {
+#' Load Koala Data
+#'
+#' This function loads the koala occurrence dataset from the package's `inst/extdata/` folder.
+#'
+#' @return A data frame containing koala occurrence records.
+#' @export
+#' @examples
+#' \dontrun{
+#' koala_data <- get_koala_data()
+#' }
+get_koala_data <- function() {
   readr::read_csv(system.file("extdata", "koala_data.csv", package = "ecotourism"))
 }
